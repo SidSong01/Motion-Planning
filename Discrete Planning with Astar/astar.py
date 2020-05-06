bf10b5b148447f2a5f197bdd7718575df12a3caf
SidSong01 <502321535@qq.com>
2020-05-06 01:22:38 -0400

    :accept:

---
+++ Discrete Planning with Astar/astar.py
@@ -0,0 +1,172 @@
+# astar.py
+
+
+
+# Compute the optimal path from start to goal.
+# The car is moving on a 2D grid and
+# its orientation can be chosen from four different directions:
+forward = [[-1,  0], # 0: go north
+           [ 0, -1], # 1: go west
+           [ 1,  0], # 2: go south
+           [ 0,  1]] # 3: go east
+
+# The car can perform 3 actions: -1: right turn and then move forward, 0: move forward, 1: left turn and then move forward
+action = [-1, 0, 1]
+action_name = ['R', 'F', 'L']
+cost = [1, 1, 10] # corresponding cost values
+
+# GRID:
+#     0 = navigable space
+#     1 = unnavigable space 
+grid = [[1, 1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1]]
+
+init = (4, 3, 0) # (grid row, grid col, orientation)
+                
+goal = (2, 0, 1) # (grid row, grid col, orientation)
+
+
+heuristic = [[2, 3, 4, 5, 6, 7], # Manhattan distance
+        [1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 4, 5],
+        [1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 6, 7]]
+
+from utils import (Value, OrderedSet, PriorityQueue)
+
+"""
+Two data structures are provided for your open and closed lists: 
+
+ 1. OrderedSet is an ordered collection of unique elements.
+ 2. PriorityQueue is a key-value container whose `pop()` method always pops out
+    the element whose value has the highest priority.
+
+ Common operations of OrderedSet, and PriorityQueue
+   len(s): number of elements in the container s
+   x in s: test x for membership in s
+   x not in s: text x for non-membership in s
+   s.clear(): clear s
+   s.remove(x): remove the element x from the set s;
+                nothing will be done if x is not in s
+
+ Unique operations of OrderedSet:
+   s.add(x): add the element x into the set s
+   s.pop(): return and remove the LAST added element in s;
+
+ Example:
+   s = Set()
+   s.add((0,1,2))    # add a triplet into the set
+   s.remove((0,1,2)) # remove the element (0,1,2) from the set
+   x = s.pop()
+
+ Unique operations of PriorityQueue:
+   PriorityQueue(order="min", f=lambda v: v): build up a priority queue
+       using the function f to compute the priority based on the value
+       of an element
+   s.put(x, v): add the element x with value v into the queue
+                update the value of x if x is already in the queue
+   s.get(x): get the value of the element x
+            raise KeyError if x is not in s
+   s.pop(): return and remove the element with highest priority in s;
+            raise IndexError if s is empty
+            if order is "min", the element with minimum f(v) will be popped;
+            if order is "max", the element with maximum f(v) will be popped.
+ Example:
+   s = PriorityQueue(order="min", f=lambda v: v.f)
+   s.put((1,1,1), Value(f=2,g=1))
+   s.put((2,2,2), Value(f=5,g=2))
+   x, v = s.pop()  # the element with minimum value of v.f will be popped
+"""
+
+# ----------------------------------------
+# ----------------------------------------
+def compute_path(grid,start,goal,cost,heuristic):
+   
+    # Use the OrderedSet for your closed list
+    closed_set = OrderedSet()
+    
+    # Use thePriorityQueue for the open list
+    open_set = PriorityQueue(order=min, f=lambda v: v.f)      
+
+    # Keep track of the parent of each node. Since the car can take 4 distinct orientations, 
+    # for each orientation we can store a 2D array indicating the grid cells. 
+    # E.g. parent[0][2][3] will denote the parent when the car is at (2,3) facing up    
+    parent = [[[' ' for row in range(len(grid[0]))] for col in range(len(grid))], 
+             [[' ' for row in range(len(grid[0]))] for col in range(len(grid))],
+             [[' ' for row in range(len(grid[0]))] for col in range(len(grid))],
+             [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]]
+
+    # The path of the car
+    path =[['-' for row in range(len(grid[0]))] for col in range(len(grid))]
+    
+    x = start[0]
+    y = start[1]
+    theta = start[2]
+    h = heuristic[x][y]
+    g = 0
+    f = g+h
+    
+    # your code: implement A*
+    open_set.put(start, Value(f=f,g=g))
+    while open_set.__len__() != 0:
+        node = open_set.pop()
+        g = node[1].g
+        x = node[0][0]
+        y = node[0][1]
+        theta = node[0][2]
+        #if reach the goal, break
+        if(node[0] == goal):
+            closed_set.add(node[0])
+            break
+        closed_set.add(node[0])
+        for idx,act in enumerate(action):
+            new_theta = (theta + act) %  4
+            #use orientation to determine the position
+            new_x = x + forward[new_theta][0]
+            new_y = y + forward[new_theta][1]
+            #to make sure it won't break the node is naviable
+            if(new_x < 0 or new_x > 4 or new_y < 0 or new_y > 5 or grid[new_x][new_y]==1):
+                continue
+            new_g = g + cost[idx]
+            new_h = heuristic[new_x][new_y]
+            new_f = new_g + new_h
+            child = [(new_x,new_y,new_theta),Value(f=new_f,g=new_g)]
+            if(child[0] not in closed_set and child[0] not in open_set):
+                open_set.put(child[0],child[1])
+                parent[new_theta][new_x][new_y] = (action_name[idx],node[0])
+            #if the cost decreased, add it to the openlist
+            elif(open_set.has(child[0]) and open_set.get(child[0]).g > new_g):
+                open_set.put(child[0],child[1])
+                parent[new_theta][new_x][new_y] = (action_name[idx],node[0])
+            #recording the path in parent
+   
+    #reach the goal
+    path[x][y] = '*'
+    #find out the path by recalling step by step
+    while (x,y,theta) != start:
+        pre_step = parent[theta][x][y]
+        x = pre_step[1][0]
+        y = pre_step[1][1]
+        theta = pre_step[1][2]
+        path[x][y] = pre_step[0]
+
+
+    # Initially you may want to ignore theta, that is, plan in 2D.
+    # To do so, set actions=forward, cost = [1, 1, 1, 1], and action_name = ['U', 'L', 'R', 'D']
+    # Similarly, set parent=[[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
+
+    return path, closed_set
+
+if __name__ == "__main__":
+    path,closed=compute_path(grid, init, goal, cost, heuristic)
+
+    for i in range(len(path)):
+        print(path[i])
+
+    print("\nExpanded Nodes")    
+    for node in closed:
+        print(node)
+
---
+++ Discrete Planning with Astar/utils.py
@@ -0,0 +1,106 @@
+# utils.py
+
+
+
+class Value():
+    """A helper class for adding f & g values to your PriorityQueue """
+
+    def __init__(self, f, g):
+        self.g = g
+        self.f = f
+
+class OrderedSet:
+    """ An ordered list of elements """
+    
+    def __init__(self):
+        self._container = []
+    
+    def add(self, item):
+        if item in self._container:
+            self._container.append(item)
+        else:
+            self._container.append(item)
+
+    def has(self, item):
+        return self._container.__contains__
+
+    def remove(self, item):
+        if item in self._container:
+            self._container.remove(item)
+    
+    def clear(self):
+        self._container.clear()
+    
+    def __contains__(self, item):
+        return self._container.__contains__(item)
+
+    def __len__(self):
+        return self._container.__len__()
+    
+    def __iter__(self):
+        return self._container.__iter__()
+    
+    def pop(self, last=True):
+        if last:
+            e = self._container.pop()
+        else:
+            e = self._container.pop(0)
+        return e
+
+class PriorityQueue:
+    """
+        A Queue in which the minimum (or maximum) element (as determined by f and
+        order) is returned first.
+    """
+    def __init__(self, order=min, f=lambda v:v):
+        if order == min or order == "min":
+            self.order = min
+        elif order == max or order == "max":
+            self.order = max
+        else:
+            raise KeyError("order must be min or max")
+        self.f = f
+
+        self._dict = {}
+  
+    def get(self, item):
+        return self._dict.__getitem__(item)
+
+    def put(self, item, value):
+        if item not in self._dict:
+            self._dict[item] = value
+        else:
+            self._dict[item] = value
+
+    def has(self, item):
+        return self._dict.__contains__(item)
+
+    def remove(self, item):
+        if item in self._dict:
+            del self._dict[item]
+
+    def pop(self):
+        if len(self._dict) > 0:
+            tar = self.order(self._dict, key=lambda k: self.f(self._dict.get(k)))
+            val = self._dict[tar]
+            del self._dict[tar]
+            return tar, val
+        raise IndexError("pop from empty priority queue")
+
+    def __iter__(self):
+        return self._dict.__iter__()
+    
+    def __contains__(self, item):
+        return self._dict.__contains__(item)
+
+    def __len__(self):
+        return self._dict.__len__()
+
+    def __getitem__(self, key):
+        return self._dict.__getitem__(key)
+    
+    def __setitem__(self, key, value):
+        return self._dict.__setitem__(key, value)
+    
+    def __delitem__(self, key):
+        return self._dict.__delitem__(key)
